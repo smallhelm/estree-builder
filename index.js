b5f14ce60c694f10007f6bf6f780bd657b010a8a
Matthew Wright <matthew@smallhelm.com>
2016-06-03 10:57:22 -0600

    number, bool, array, json

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ var has = function(obj, key){
   return obj != null && hasOwnProperty.call(obj, key);
 };
 
+var isObject = function(val){
+  return !!val && (typeof val == 'object');
+};
+
 var isLocationNode = function(loc){
   return loc && has(loc, 'start') && has(loc, 'end');
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
-var e = {};
+var e = module.exports = {};
 
 var def = function(names, builder){
   if(typeof names === 'string'){
@@ -36,11 +40,72 @@ var def = function(names, builder){
   });
 };
 
-def(['string', 'str'], function(val){
+def(['string', 'str', 'number', 'num', 'float'], function(val){
   return {
     'type': 'Literal',
-    'value': val//string | boolean | null | number
+    'value': val
   };
 });
 
-module.exports = e;
+def('null', function(){
+  return {
+    'type': 'Literal',
+    'value': null
+  };
+});
+
+def('true', function(){
+  return {
+    'type': 'Literal',
+    'value': true
+  };
+});
+
+def('false', function(){
+  return {
+    'type': 'Literal',
+    'value': false
+  };
+});
+
+def(['array', 'arr'], function(elements){
+  return {
+    type: 'ArrayExpression',
+    elements: elements
+  };
+});
+
+def(['object', 'obj'], function(pairs){
+  return {
+    type: 'ObjectExpression',
+    properties: pairs.map(function(pair){
+      return {
+        type: 'Property',
+        key: pair[0],
+        value: pair[1],
+        kind: 'init'
+      };
+    })
+  };
+});
+
+e.json = function(val, loc){
+  if(Array.isArray(val)){
+    return e.array(val.map(function(elm){
+      return e.json(elm, loc);
+    }));
+  }
+  if(isObject(val)){
+    return e.object(val, loc);
+  }
+  if(val === true || val === false){
+    return e[val ? 'true' : 'false'](loc);
+  }
+  if(typeof val === 'string'){
+    return e.string(val, loc);
+  }
+  if(typeof val === 'number'){
+    return e.number(val, loc);
+  }
+  return e['null']();
+};
--- tests.js
+++ tests.js
@@ -4,5 +4,6 @@ var astring = require('astring');
 
 test('basics', function(t){
   t.equals(astring(e.string('blah')), '"blah"');
+  t.equals(astring(e.json([2, '3', true, false, null])), '[2, "3", true, false, null]');
   t.end();
 });
